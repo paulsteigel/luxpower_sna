4cdfa8930d3201b85cb4a32c489aba5e5bec5279
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-08 06:54:38 +0000

    commit

--- components/luxpower_sna/sensor.h
+++ components/luxpower_sna/sensor.h
@@ -1,42 +1,27 @@
-// custom_components/luxpower_inverter/sensor.h
 #pragma once
-
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/core/log.h"
-#include "esphome/core/helpers.h"
-
-// Forward declaration of the main component
-namespace esphome {
-namespace luxpower_inverter {
-class LuxPowerInverterComponent; // Forward declaration
-enum LuxpowerRegType; // Forward declaration of the enum
-} // namespace luxpower_inverter
-} // namespace esphome
+#include "esphome/core/component.h"
+#include "sensor/sensor.h" // MODIFIED LINE
+#include "luxpower_inverter.h"
 
 namespace esphome {
-namespace luxpower_inverter {
+namespace luxpower_sna {
 
-// --- Custom Luxpower Sensor Class ---
-class LuxpowerSensor : public sensor::Sensor {
+class LuxpowerSensor : public sensor::Sensor, public Component {
  public:
-  void set_parent(LuxPowerInverterComponent *parent) { this->parent_ = parent; }
-  void set_register_address(uint16_t addr) { this->register_address_ = addr; }
-  void set_reg_type(LuxpowerRegType type) { this->reg_type_ = type; }
-  void set_bank(uint8_t bank) { this->bank_ = bank; }
+  LuxpowerSensor(const std::string &name, uint16_t register_address, LuxpowerRegType reg_type, uint8_t bank);
 
-  // Getters for debugging/logging
-  uint16_t get_register_address() const { return this->register_address_; }
-  uint8_t get_bank() const { return this->bank_; }
+  void setup() override;
+  void dump_config() override;
+  void loop() override;
 
-  // Method to update this sensor's state from the component's parsed data
-  void update_state(uint16_t raw_value);
+  void set_parent(LuxPowerInverterComponent *parent) { parent_ = parent; }
 
  protected:
-  LuxPowerInverterComponent *parent_; // Pointer to the main component
+  LuxPowerInverterComponent *parent_;
   uint16_t register_address_;
   LuxpowerRegType reg_type_;
   uint8_t bank_;
 };
 
-} // namespace luxpower_inverter
+} // namespace luxpower_sna
 } // namespace esphome
