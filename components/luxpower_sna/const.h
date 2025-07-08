bd46b4ccdc15067e33c63c0662bd8ada4419a64e
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-08 22:45:48 +0700

    Update const.h

--- components/luxpower_sna/const.h
+++ components/luxpower_sna/const.h
@@ -3,7 +3,7 @@
 namespace esphome {
 namespace luxpower_sna {
 
-// From LXPPacket.py
+// From original Python library (LXPPacket.py)
 const uint8_t TCP_FUNCTION_HEARTBEAT = 193;
 const uint8_t TCP_FUNCTION_TRANSLATED_DATA = 194;
 
@@ -12,7 +12,7 @@ const uint8_t DEVICE_FUNCTION_READ_INPUT = 4;
 const uint8_t DEVICE_FUNCTION_WRITE_SINGLE = 6;
 
 const uint8_t ACTION_READ = 1;
-const uint8_t ACTION_WRITE = 0;
+const uint8_t ACTION_WRITE = 0; // The protocol uses this for read requests
 
 }  // namespace luxpower_sna
 }  // namespace esphome
