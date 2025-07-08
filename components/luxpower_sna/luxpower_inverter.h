b5b6664688052262b4e84123a3f00fd9ec045e9d
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-08 23:20:51 +0700

    Update luxpower_inverter.h

--- components/luxpower_sna/luxpower_inverter.h
+++ components/luxpower_sna/luxpower_inverter.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
+//#include "esphome/components/sensor/sensor.h"
 #include "esphome/core/log.h"
 #include <vector>
 #include "ESPAsyncTCP.h"
