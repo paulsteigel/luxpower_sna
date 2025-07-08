0c6c38704067e44b59b76ff6d406e1cfa0cf41ac
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-08 07:09:55 +0000

    commit

--- components/luxpower_sna/luxpower_inverter.h
+++ components/luxpower_sna/luxpower_inverter.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
-#include "sensor/luxpower_sna_sensor.h" // MODIFIED: Now points to the renamed file in the subfolder
+#include "sensor/sensor.h" // MODIFIED: Now points to the renamed file in the subfolder
 #include <vector>
 #include <string>
 #include <chrono>
