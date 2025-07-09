f88bbcf2ffb29cd8a2024f13369ff8efd09bef75
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-09 09:27:58 +0700

    Update luxpower_sna.h

--- components/luxpower_sna/luxpower_sna.h
+++ components/luxpower_sna/luxpower_sna.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "esphome/core/component.hh"
+#include "esphome/core/component.h"
 #include "esphome/components/socket/socket.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
