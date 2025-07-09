2ac753b9aaf1eef3d74cd6f480cfc0d3a86d2c29
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-09 23:56:12 +0700

    Update __init__.py

--- components/luxpower_sna/__init__.py
+++ components/luxpower_sna/__init__.py
@@ -2,7 +2,7 @@
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_HOST, CONF_PORT
+from esphome.const import CONF_ID
 
 # Define the namespace for our C++ code
 luxpower_sna_ns = cg.esphome_ns.namespace("esphome::luxpower_sna")
@@ -12,14 +12,15 @@ LuxpowerSNAComponent = luxpower_sna_ns.class_("LuxpowerSNAComponent", cg.Polling
 CONF_LUXPOWER_SNA_ID = "luxpower_sna_id"
 
 # Define the YAML schema for the main `luxpower_sna:` block
+# Using string literals "host" and "port" is the correct way
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(LuxpowerSNAComponent),
-        cv.Required(CONF_HOST): cv.string,
-        cv.Required(CONF_PORT): cv.port,
+        cv.Required("host"): cv.string,
+        cv.Required("port"): cv.port,
         cv.Required("dongle_serial"): cv.string,
     }
-).extend(cv.polling_component_schema("10s"))
+).extend(cv.polling_component_schema("20s")) # Also corrected the update interval here to match your YAML
 
 # Define a separate schema that other platforms (like sensor) can use to link back
 LUXPOWER_SNA_COMPONENT_SCHEMA = cv.Schema(
@@ -33,6 +34,8 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_host(config[CONF_HOST]))
-    cg.add(var.set_port(config[CONF_PORT]))
+    # Use string literals to access the config values
+    cg.add(var.set_host(config["host"]))
+    cg.add(var.set_port(config["port"]))
     cg.add(var.set_dongle_serial(config["dongle_serial"]))
+
