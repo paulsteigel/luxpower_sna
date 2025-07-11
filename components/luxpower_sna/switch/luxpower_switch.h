5687fb94e61a4c09c0d0b3b7cc5508db3c0bbf9e
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-11 23:19:54 +0700

    Update luxpower_switch.h

--- components/luxpower_sna/switch/luxpower_switch.h
+++ components/luxpower_sna/switch/luxpower_switch.h
@@ -1,17 +1,26 @@
-#include "luxpower_switch.h"
-#include "esphome/core/log.h"
-#include "esphome/core/application.h"
+#pragma once
+
+#include "../jk_bms.h"
+#include "esphome/core/component.h"
+#include "esphome/components/switch/switch.h"
 
 namespace esphome {
-namespace luxpower {
+namespace jk_bms {
 
-static const char *const TAG = "jk_bms.switch";
+class JkBms;
+class JkSwitch : public switch_::Switch, public Component {
+ public:
+  void set_parent(JkBms *parent) { this->parent_ = parent; };
+  void set_holding_register(uint8_t holding_register) { this->holding_register_ = holding_register; };
+  void dump_config() override;
+  void loop() override {}
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
-void LuxpowerSwitch::dump_config() { LOG_SWITCH("", "Luxpower Switch", this); }
-void LuxpowerSwitch::write_state(bool state) {
-  this->parent_->write_register(this->holding_register_, (uint8_t) state);
-  this->publish_state(state);
-}
+ protected:
+  void write_state(bool state) override;
+  JkBms *parent_;
+  uint8_t holding_register_;
+};
 
-}  // namespace LuxpowerSwitch
+}  // namespace jk_bms
 }  // namespace esphome
