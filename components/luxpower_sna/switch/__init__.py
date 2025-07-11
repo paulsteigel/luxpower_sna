71ccf2ae0db3852d4adfbf98b59ef49758872f50
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-11 23:38:19 +0700

    Update __init__.py

--- components/luxpower_sna/switch/__init__.py
+++ components/luxpower_sna/switch/__init__.py
@@ -1,54 +1,53 @@
+# components/luxpower_sna/__init__.py
 import esphome.codegen as cg
-from esphome.components import switch
 import esphome.config_validation as cv
+from esphome.core import CORE, coroutine_with_priority
 from esphome.const import CONF_ID
 
-from .. import CONF_JK_BMS_ID, JK_BMS_COMPONENT_SCHEMA, jk_bms_ns
-from ..const import CONF_BALANCER, CONF_CHARGING, CONF_DISCHARGING
+DEPENDENCIES = ["wifi"]
+AUTO_LOAD = ["sensor", "text_sensor", "switch"] # <<< MODIFIED: Add switch
+MULTI_CONF = True
 
-DEPENDENCIES = ["jk_bms"]
+luxpower_sna_ns = cg.esphome_ns.namespace("luxpower_sna")
+LuxpowerSNAComponent = luxpower_sna_ns.class_("LuxpowerSNAComponent", cg.PollingComponent)
+# <<< NEW: Forward declare the switch class
+LuxpowerSNASwitch = luxpower_sna_ns.class_("LuxpowerSNASwitch", cg.Component)
 
-CODEOWNERS = ["@syssi"]
 
-ICON_CHARGING = "mdi:battery-charging-50"
-ICON_DISCHARGING = "mdi:battery-charging-50"
-ICON_BALANCER = "mdi:seesaw"
+CONF_LUXPOWER_SNA_ID = "luxpower_sna_id"
 
-SWITCHES = {
-    CONF_CHARGING: 0xAB,
-    CONF_DISCHARGING: 0xAC,
-    # The BMS (v11) doesn't accept updates of register 0x9D at the moment
-    CONF_BALANCER: 0x9D,
-}
-
-JkSwitch = jk_bms_ns.class_("JkSwitch", switch.Switch, cg.Component)
-
-CONFIG_SCHEMA = JK_BMS_COMPONENT_SCHEMA.extend(
+LUXPOWER_SNA_COMPONENT_SCHEMA = cv.Schema(
     {
-        cv.Optional(CONF_CHARGING): switch.switch_schema(
-            JkSwitch,
-            icon=ICON_CHARGING,
-        ).extend(cv.COMPONENT_SCHEMA),
-        cv.Optional(CONF_DISCHARGING): switch.switch_schema(
-            JkSwitch,
-            icon=ICON_DISCHARGING,
-        ).extend(cv.COMPONENT_SCHEMA),
-        # cv.Optional(CONF_BALANCER): switch.switch_schema(
-        #     JkSwitch,
-        #     icon=ICON_BALANCER,
-        # ).extend(cv.COMPONENT_SCHEMA),
+        cv.GenerateID(CONF_LUXPOWER_SNA_ID): cv.use_id(LuxpowerSNAComponent),
     }
 )
 
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(LuxpowerSNAComponent),
+            cv.Required("host"): cv.string,
+            cv.Required("port"): cv.port,
+            cv.Required("dongle_serial"): cv.string,
+            cv.Required("inverter_serial_number"): cv.string,
+        }
+    )
+    .extend(cv.polling_component_schema("20s"))
+)
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_JK_BMS_ID])
-    for key, address in SWITCHES.items():
-        if key in config:
-            conf = config[key]
-            var = cg.new_Pvariable(conf[CONF_ID])
-            await cg.register_component(var, conf)
-            await switch.register_switch(var, conf)
-            cg.add(getattr(hub, f"set_{key}_switch")(var))
-            cg.add(var.set_parent(hub))
-            cg.add(var.set_holding_register(address))
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    cg.add(var.set_host(config["host"]))
+    cg.add(var.set_port(config["port"]))
+
+    dongle_serial = config["dongle_serial"]
+    if len(dongle_serial) != 10:
+        raise cv.Invalid("dongle_serial must be 10 characters long")
+    cg.add(var.set_dongle_serial(dongle_serial))
+
+    inverter_serial = config["inverter_serial_number"]
+    if len(inverter_serial) != 10:
+        raise cv.Invalid("inverter_serial_number must be 10 characters long")
+    cg.add(var.set_inverter_serial_number(inverter_serial))
