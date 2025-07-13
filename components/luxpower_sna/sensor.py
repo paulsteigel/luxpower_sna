de359643bb8241205dd97d91c9cf35abd520632a
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-13 13:25:59 +0700

    Update sensor.py

--- components/luxpower_sna/sensor.py
+++ components/luxpower_sna/sensor.py
@@ -64,7 +64,7 @@ YAML_TO_C_NAMES = {
     "ct_clamp_live": "ct_clamp_live",
     "home_consumption2": "home_consumption2",
     "total_pv_energy": "total_pv_energy",
-    "e_pv_total_today": "daily_solar",
+    "daily_solar": "daily_solar",
     "home_consumption_total": "home_consumption_total",
     "home_consumption_live": "home_consumption_live",        
     "battery_flow": "battery_flow",
@@ -165,7 +165,7 @@ SENSOR_TYPES = {
     "battery_flow": sensor.sensor_schema(unit_of_measurement=UNIT_WATT, device_class=DEVICE_CLASS_ENERGY, state_class=STATE_CLASS_TOTAL_INCREASING, accuracy_decimals=2),
     "grid_flow": sensor.sensor_schema(unit_of_measurement=UNIT_WATT, device_class=DEVICE_CLASS_ENERGY, state_class=STATE_CLASS_TOTAL_INCREASING, accuracy_decimals=2),
     "home_consumption_live": sensor.sensor_schema(unit_of_measurement=UNIT_WATT, device_class=DEVICE_CLASS_ENERGY, state_class=STATE_CLASS_TOTAL_INCREASING, accuracy_decimals=1, icon="mdi:home-lightning-bolt-outline"),
-    "e_pv_total_today": sensor.sensor_schema(unit_of_measurement=UNIT_KILOWATT_HOURS, device_class=DEVICE_CLASS_ENERGY, state_class=STATE_CLASS_TOTAL_INCREASING, accuracy_decimals=1, icon="mdi:home-lightning-bolt-outline"),
+    "daily_solar": sensor.sensor_schema(unit_of_measurement=UNIT_KILOWATT_HOURS, device_class=DEVICE_CLASS_ENERGY, state_class=STATE_CLASS_TOTAL_INCREASING, accuracy_decimals=1, icon="mdi:home-lightning-bolt-outline"),
     
     # Text sensor schemas
     "status_text": text_sensor.text_sensor_schema(icon="mdi:information-outline"),
