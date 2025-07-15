3fec7d4e9b3aa621297447bd10f499ffd09eafcd
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-15 16:14:51 +0700

    Update luxpower_sna.h

--- components/luxpower_sna/luxpower_sna.h
+++ components/luxpower_sna/luxpower_sna.h
@@ -19,36 +19,7 @@ namespace luxpower_sna {
 
 static const char *const TAG = "luxpower_sna";
 
-// =========================================================================
-// The Client class to handle persistent connections
-// =========================================================================
-class Client {
- public:
-  Client() = default;
-
-  // Public interface for the main component to use.
-  // These are DECLARED here and DEFINED in the .cpp file.
-  void setup(const std::string &host, uint16_t port);
-  void loop();
-  bool is_connected();
-  size_t write(const uint8_t *buffer, size_t size);
-  int read(uint8_t *buffer, size_t size);
-  int available();
-
- private:
-  enum State { STATE_DISCONNECTED, STATE_CONNECTING, STATE_CONNECTED };
-  void connect_();
-  void disconnect_();
-
-  WiFiClient client_;
-  State state_{STATE_DISCONNECTED};
-  std::string host_;
-  uint16_t port_;
-  uint32_t last_connect_attempt_{0};
-};
-
 #pragma pack(push, 1)
-// --- YOUR ORIGINAL STRUCTS: UNCHANGED ---
 struct LuxHeader {
   uint16_t prefix; uint16_t protocolVersion; uint16_t packetLength; uint8_t address;
   uint8_t function; char serialNumber[10]; uint16_t dataLength;
@@ -88,18 +59,20 @@ class LuxpowerSNAComponent : public PollingComponent {
   void setup() override;
   void dump_config() override;
   void update() override;
-  void loop() override;
 
   void set_host(const std::string &host) { host_ = host; }
   void set_port(uint16_t port) { port_ = port; }
   void set_dongle_serial(const std::string &serial) { dongle_serial_ = serial; }
   void set_inverter_serial(const std::string &serial) { inverter_serial_ = serial; }
 
-  // All sensor setters are unchanged
+  // System Sensor Setters
   void set_lux_firmware_version_sensor(text_sensor::TextSensor *s) { lux_firmware_version_sensor_ = s; }
   void set_lux_inverter_model_sensor(text_sensor::TextSensor *s) { lux_inverter_model_sensor_ = s; }
   void set_lux_status_text_sensor(text_sensor::TextSensor *s) { lux_status_text_sensor_ = s; }
   void set_lux_battery_status_text_sensor(text_sensor::TextSensor *s) { lux_battery_status_text_sensor_ = s; }
+  void set_inverter_serial_number_sensor(text_sensor::TextSensor *s) { inverter_serial_number_sensor_ = s; }
+
+  // Section1 Sensor Setters
   void set_lux_current_solar_voltage_1_sensor(sensor::Sensor *s) { lux_current_solar_voltage_1_sensor_ = s; }
   void set_lux_current_solar_voltage_2_sensor(sensor::Sensor *s) { lux_current_solar_voltage_2_sensor_ = s; }
   void set_lux_current_solar_voltage_3_sensor(sensor::Sensor *s) { lux_current_solar_voltage_3_sensor_ = s; }
@@ -147,6 +120,8 @@ class LuxpowerSNAComponent : public PollingComponent {
   void set_lux_grid_flow_sensor(sensor::Sensor *s) { lux_grid_flow_sensor_ = s; }
   void set_lux_home_consumption_live_sensor(sensor::Sensor *s) { lux_home_consumption_live_sensor_ = s; }
   void set_lux_home_consumption_sensor(sensor::Sensor *s) { lux_home_consumption_sensor_ = s; }
+
+  // Section2 Sensor Setters
   void set_lux_total_solar_array_1_sensor(sensor::Sensor *s) { lux_total_solar_array_1_sensor_ = s; }
   void set_lux_total_solar_array_2_sensor(sensor::Sensor *s) { lux_total_solar_array_2_sensor_ = s; }
   void set_lux_total_solar_array_3_sensor(sensor::Sensor *s) { lux_total_solar_array_3_sensor_ = s; }
@@ -166,6 +141,8 @@ class LuxpowerSNAComponent : public PollingComponent {
   void set_lux_uptime_sensor(sensor::Sensor *s) { lux_uptime_sensor_ = s; }
   void set_lux_total_solar_sensor(sensor::Sensor *s) { lux_total_solar_sensor_ = s; }
   void set_lux_home_consumption_total_sensor(sensor::Sensor *s) { lux_home_consumption_total_sensor_ = s; }
+
+  // Section3 Sensor Setters
   void set_lux_bms_limit_charge_sensor(sensor::Sensor *s) { lux_bms_limit_charge_sensor_ = s; }
   void set_lux_bms_limit_discharge_sensor(sensor::Sensor *s) { lux_bms_limit_discharge_sensor_ = s; }
   void set_charge_voltage_ref_sensor(sensor::Sensor *s) { charge_voltage_ref_sensor_ = s; }
@@ -180,6 +157,8 @@ class LuxpowerSNAComponent : public PollingComponent {
   void set_min_cell_temp_sensor(sensor::Sensor *s) { min_cell_temp_sensor_ = s; }
   void set_lux_battery_cycle_count_sensor(sensor::Sensor *s) { lux_battery_cycle_count_sensor_ = s; }
   void set_lux_home_consumption_2_live_sensor(sensor::Sensor *s) { lux_home_consumption_2_live_sensor_ = s; }
+
+  // Section4 Sensor Setters
   void set_lux_current_generator_voltage_sensor(sensor::Sensor *s) { lux_current_generator_voltage_sensor_ = s; }
   void set_lux_current_generator_frequency_sensor(sensor::Sensor *s) { lux_current_generator_frequency_sensor_ = s; }
   void set_lux_current_generator_power_sensor(sensor::Sensor *s) { lux_current_generator_power_sensor_ = s; }
@@ -189,6 +168,8 @@ class LuxpowerSNAComponent : public PollingComponent {
   void set_lux_current_eps_L2_voltage_sensor(sensor::Sensor *s) { lux_current_eps_L2_voltage_sensor_ = s; }
   void set_lux_current_eps_L1_watt_sensor(sensor::Sensor *s) { lux_current_eps_L1_watt_sensor_ = s; }
   void set_lux_current_eps_L2_watt_sensor(sensor::Sensor *s) { lux_current_eps_L2_watt_sensor_ = s; }
+
+  // Section5 Sensor Setters
   void set_p_load_ongrid_sensor(sensor::Sensor *s) { p_load_ongrid_sensor_ = s; }
   void set_e_load_day_sensor(sensor::Sensor *s) { e_load_day_sensor_ = s; }
   void set_e_load_all_l_sensor(sensor::Sensor *s) { e_load_all_l_sensor_ = s; }
@@ -205,22 +186,18 @@ class LuxpowerSNAComponent : public PollingComponent {
   void publish_sensor_(sensor::Sensor *sensor, float value);
   void publish_text_sensor_(text_sensor::TextSensor *sensor, const std::string &value);
 
-  // --- Member Variables ---
   std::string host_;
   uint16_t port_;
   std::string dongle_serial_;
   std::string inverter_serial_;
   
-  // This is our new connection manager client
-  Client client_;
+  WiFiClient client_;
 
-  uint8_t next_bank_index_{0};
   const uint8_t banks_[5] = {0, 40, 80, 120, 160};
 
   static const char *STATUS_TEXTS[193];
   static const char *BATTERY_STATUS_TEXTS[17];
 
-  // All sensor pointers are unchanged
   text_sensor::TextSensor *lux_firmware_version_sensor_{nullptr}, *lux_inverter_model_sensor_{nullptr}, *lux_status_text_sensor_{nullptr}, *lux_battery_status_text_sensor_{nullptr}, *inverter_serial_number_sensor_{nullptr};
   sensor::Sensor *lux_current_solar_voltage_1_sensor_{nullptr}, *lux_current_solar_voltage_2_sensor_{nullptr}, *lux_current_solar_voltage_3_sensor_{nullptr}, *lux_battery_voltage_sensor_{nullptr}, *lux_battery_percent_sensor_{nullptr}, *soh_sensor_{nullptr}, *lux_internal_fault_sensor_{nullptr}, *lux_current_solar_output_1_sensor_{nullptr}, *lux_current_solar_output_2_sensor_{nullptr}, *lux_current_solar_output_3_sensor_{nullptr}, *lux_battery_charge_sensor_{nullptr}, *lux_battery_discharge_sensor_{nullptr}, *lux_grid_voltage_r_sensor_{nullptr}, *lux_grid_voltage_s_sensor_{nullptr}, *lux_grid_voltage_t_sensor_{nullptr}, *lux_grid_frequency_live_sensor_{nullptr}, *lux_grid_voltage_live_sensor_{nullptr}, *lux_power_from_inverter_live_sensor_{nullptr}, *lux_power_to_inverter_live_sensor_{nullptr}, *lux_power_current_clamp_sensor_{nullptr}, *grid_power_factor_sensor_{nullptr}, *eps_voltage_r_sensor_{nullptr}, *eps_voltage_s_sensor_{nullptr}, *eps_voltage_t_sensor_{nullptr}, *eps_frequency_sensor_{nullptr}, *lux_power_to_eps_sensor_{nullptr}, *lux_power_to_grid_live_sensor_{nullptr}, *lux_power_from_grid_live_sensor_{nullptr}, *lux_daily_solar_array_1_sensor_{nullptr}, *lux_daily_solar_array_2_sensor_{nullptr}, *lux_daily_solar_array_3_sensor_{nullptr}, *lux_power_from_inverter_daily_sensor_{nullptr}, *lux_power_to_inverter_daily_sensor_{nullptr}, *lux_daily_battery_charge_sensor_{nullptr}, *lux_daily_battery_discharge_sensor_{nullptr}, *lux_power_to_eps_daily_sensor_{nullptr}, *lux_power_to_grid_daily_sensor_{nullptr}, *lux_power_from_grid_daily_sensor_{nullptr}, *bus1_voltage_sensor_{nullptr}, *bus2_voltage_sensor_{nullptr}, *lux_current_solar_output_sensor_{nullptr}, *lux_daily_solar_sensor_{nullptr}, *lux_power_to_home_sensor_{nullptr}, *lux_battery_flow_sensor_{nullptr}, *lux_grid_flow_sensor_{nullptr}, *lux_home_consumption_live_sensor_{nullptr}, *lux_home_consumption_sensor_{nullptr}, *lux_total_solar_array_1_sensor_{nullptr}, *lux_total_solar_array_2_sensor_{nullptr}, *lux_total_solar_array_3_sensor_{nullptr}, *lux_power_from_inverter_total_sensor_{nullptr}, *lux_power_to_inverter_total_sensor_{nullptr}, *lux_total_battery_charge_sensor_{nullptr}, *lux_total_battery_discharge_sensor_{nullptr}, *lux_power_to_eps_total_sensor_{nullptr}, *lux_power_to_grid_total_sensor_{nullptr}, *lux_power_from_grid_total_sensor_{nullptr}, *lux_fault_code_sensor_{nullptr}, *lux_warning_code_sensor_{nullptr}, *lux_internal_temp_sensor_{nullptr}, *lux_radiator1_temp_sensor_{nullptr}, *lux_radiator2_temp_sensor_{nullptr}, *lux_battery_temperature_live_sensor_{nullptr}, *lux_uptime_sensor_{nullptr}, *lux_total_solar_sensor_{nullptr}, *lux_home_consumption_total_sensor_{nullptr}, *lux_bms_limit_charge_sensor_{nullptr}, *lux_bms_limit_discharge_sensor_{nullptr}, *charge_voltage_ref_sensor_{nullptr}, *discharge_cutoff_voltage_sensor_{nullptr}, *battery_status_inv_sensor_{nullptr}, *lux_battery_count_sensor_{nullptr}, *lux_battery_capacity_ah_sensor_{nullptr}, *lux_battery_current_sensor_{nullptr}, *max_cell_volt_sensor_{nullptr}, *min_cell_volt_sensor_{nullptr}, *max_cell_temp_sensor_{nullptr}, *min_cell_temp_sensor_{nullptr}, *lux_battery_cycle_count_sensor_{nullptr}, *lux_home_consumption_2_live_sensor_{nullptr}, *lux_current_generator_voltage_sensor_{nullptr}, *lux_current_generator_frequency_sensor_{nullptr}, *lux_current_generator_power_sensor_{nullptr}, *lux_current_generator_power_daily_sensor_{nullptr}, *lux_current_generator_power_all_sensor_{nullptr}, *lux_current_eps_L1_voltage_sensor_{nullptr}, *lux_current_eps_L2_voltage_sensor_{nullptr}, *lux_current_eps_L1_watt_sensor_{nullptr}, *lux_current_eps_L2_watt_sensor_{nullptr}, *p_load_ongrid_sensor_{nullptr}, *e_load_day_sensor_{nullptr}, *e_load_all_l_sensor_{nullptr};
 };
