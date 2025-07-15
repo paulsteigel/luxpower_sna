dcb8302d1cdb008a98b3bec7b6e26b73774c493b
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-07-15 21:35:33 +0700

    Update __init__.py

--- components/luxclient/__init__.py
+++ components/luxclient/__init__.py
@@ -1,18 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import wifi # Changed from 'network' to 'wifi'
+from esphome.components import wifi
 from esphome.const import (
     CONF_ID,
-    CONF_HOST,
     CONF_PORT,
     CONF_TIMEOUT,
 )
 
-# DEPENDENCIES is now 'wifi'
 DEPENDENCIES = ["wifi"]
 
 luxclient_ns = cg.esphome_ns.namespace("luxclient")
 LuxClient = luxclient_ns.class_("LuxClient", cg.Component)
+
+# --- START OF FIX ---
+# Define CONF_HOST ourselves, as it's not in esphome.const
+CONF_HOST = "host"
+# --- END OF FIX ---
 
 CONF_DONGLE_SERIAL = "dongle_serial"
 CONF_INVERTER_SERIAL = "inverter_serial_number"
@@ -27,7 +30,7 @@ def validate_serial(value):
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(LuxClient),
-        cv.Required(CONF_HOST): cv.string,
+        cv.Required(CONF_HOST): cv.string, # Now uses our locally defined CONF_HOST
         cv.Optional(CONF_PORT, default=8000): cv.port,
         cv.Required(CONF_DONGLE_SERIAL): validate_serial,
         cv.Required(CONF_INVERTER_SERIAL): validate_serial,
@@ -42,11 +45,11 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_host(config[CONF_HOST]))
+    cg.add(var.set_host(config[CONF_HOST])) # Now uses our locally defined CONF_HOST
     cg.add(var.set_port(config[CONF_PORT]))
     cg.add(var.set_dongle_serial(config[CONF_DONGLE_SERIAL]))
     cg.add(var.set_inverter_serial(config[CONF_INVERTER_SERIAL]))
     cg.add(var.set_read_timeout(config[CONF_TIMEOUT]))
 
-    # We can now be certain that WiFi is available and add the library.
     cg.add_library("WiFi", None)
+
