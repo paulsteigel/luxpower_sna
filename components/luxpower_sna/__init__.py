605b6ba867d7747cdf8819c76dd5610a099673c0
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-08-27 17:02:32 +0700

    Update __init__.py

--- components/luxpower_sna/__init__.py
+++ components/luxpower_sna/__init__.py
@@ -5,13 +5,19 @@ from esphome.core import CORE, coroutine_with_priority
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["wifi"]
-AUTO_LOAD = ["sensor", "text_sensor"] # Make sure text_sensor is also loaded
+AUTO_LOAD = ["sensor", "text_sensor"]
 MULTI_CONF = True
 
 luxpower_sna_ns = cg.esphome_ns.namespace("luxpower_sna")
 LuxpowerSNAComponent = luxpower_sna_ns.class_("LuxpowerSNAComponent", cg.PollingComponent)
 
 CONF_LUXPOWER_SNA_ID = "luxpower_sna_id"
+
+# Configuration parameter keys that reference input IDs
+CONF_HOST = "host"
+CONF_PORT = "port" 
+CONF_DONGLE_SERIAL = "dongle_serial"
+CONF_INVERTER_SERIAL = "inverter_serial"
 
 LUXPOWER_SNA_COMPONENT_SCHEMA = cv.Schema(
     {
@@ -23,39 +29,27 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(LuxpowerSNAComponent),
-            cv.Required("host"): cv.string,
-            cv.Required("port"): cv.port,
-            cv.Required("dongle_serial"): cv.string,
-            cv.Required("inverter_serial_number"): cv.string,
+            cv.Required(CONF_HOST): cv.use_id(cg.std_string),  # Reference to text input ID
+            cv.Required(CONF_PORT): cv.use_id(cg.float_),      # Reference to number input ID
+            cv.Required(CONF_DONGLE_SERIAL): cv.use_id(cg.std_string),    # Reference to text input ID
+            cv.Required(CONF_INVERTER_SERIAL): cv.use_id(cg.std_string),  # Reference to text input ID
         }
     )
     .extend(cv.polling_component_schema("20s"))
 )
 
 async def to_code(config):
+    """Generate C++ code for the component"""
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_host(config["host"]))
-    cg.add(var.set_port(config["port"]))
-
-    # --- Corrected Code Generation ---
-    # We pass the string directly from the config.
-    # The C++ side will handle validation and conversion.
-    dongle_serial = config["dongle_serial"]
-    if len(dongle_serial) != 10:
-        raise cv.Invalid("dongle_serial must be 10 characters long")
-    cg.add(var.set_dongle_serial(dongle_serial))
-
-    inverter_serial = config["inverter_serial_number"]
-    if len(inverter_serial) != 10:
-        raise cv.Invalid("inverter_serial_number must be 10 characters long")
-    # This call must match the function name in the .h file exactly.
-    cg.add(var.set_inverter_serial(inverter_serial))
+    # Set references to input components
+    host_var = await cg.get_variable(config[CONF_HOST])
+    port_var = await cg.get_variable(config[CONF_PORT])
+    dongle_serial_var = await cg.get_variable(config[CONF_DONGLE_SERIAL])
+    inverter_serial_var = await cg.get_variable(config[CONF_INVERTER_SERIAL])
     
-    #if CORE.is_esp32 or CORE.is_libretiny:
-    #    # https://github.com/ESP32Async/AsyncTCP
-    #    cg.add_library("ESP32Async/AsyncTCP", "3.4.4")
-    #elif CORE.is_esp8266:
-    #    # https://github.com/ESP32Async/ESPAsyncTCP
-    #    cg.add_library("ESP32Async/ESPAsyncTCP", "2.0.0")
+    cg.add(var.set_host_input(host_var))
+    cg.add(var.set_port_input(port_var))
+    cg.add(var.set_dongle_serial_input(dongle_serial_var))
+    cg.add(var.set_inverter_serial_input(inverter_serial_var))
