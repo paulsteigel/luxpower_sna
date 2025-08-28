ea39e5c7f955f7a4a793ffa016c3391783af1336
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2025-08-28 09:07:14 +0700

    Update luxpower_sna.h

--- components/luxpower_sna/luxpower_sna.h
+++ components/luxpower_sna/luxpower_sna.h
@@ -126,8 +126,9 @@ class LuxpowerSNAComponent : public PollingComponent {
   // Methods for switch support
   void read_register_async(uint16_t reg, std::function<void(uint16_t)> callback);
   void write_register_async(uint16_t reg, uint16_t value, std::function<void(bool)> callback);
+  
   bool is_connection_ready() const {
-    return this->connection_state_ == ConnectionState::CONNECTED;
+    return connection_state_ == ConnectionState::CONNECTED && !processing_async_request_;
   }
   
   // Alternative method names in case you're using different naming
