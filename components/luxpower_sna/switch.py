56ee98cb79189ef2502a9d2e55c5b341bb0058ad
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2026-03-15 20:43:43 +0700

    Update switch.py

--- components/luxpower_sna/switch.py
+++ components/luxpower_sna/switch.py
@@ -10,37 +10,29 @@ Example YAML:
       luxpower_sna_id: lux_hub
       name: "AC Charge Enable"
       register: 21
-      bitmask: 0x0080   # LXPPacket.AC_CHARGE_ENABLE
+      bitmask: 0x0080
       icon: "mdi:battery-charging"
 
-Pre-defined bitmasks (reg 21) from HA integration LXPPacket.py:
-  NORMAL_OR_STANDBY      = 0x0200
-  POWER_BACKUP_ENABLE    = 0x0001
-  FEED_IN_GRID           = 0x8000
-  SEAMLESS_EPS_SWITCHING = 0x0100
-  AC_CHARGE_ENABLE       = 0x0080
-  CHARGE_PRIORITY        = 0x0800
-  FORCED_DISCHARGE_ENABLE= 0x0400
-  DCI_ENABLE             = 0x4000
-  GFCI_ENABLE            = 0x2000
-  GRID_ON_POWER_SS       = 0x0040
-  NEUTRAL_DETECT_ENABLE  = 0x0020
-  ANTI_ISLAND_ENABLE     = 0x0010
-  DRMS_ENABLE            = 0x0004
-  OVF_LOAD_DERATE_ENABLE = 0x0002
+Bitmasks (reg 21):
+  NORMAL_OR_STANDBY       = 0x0200    POWER_BACKUP_ENABLE     = 0x0001
+  FEED_IN_GRID            = 0x8000    SEAMLESS_EPS_SWITCHING  = 0x0100
+  AC_CHARGE_ENABLE        = 0x0080    CHARGE_PRIORITY         = 0x0800
+  FORCED_DISCHARGE_ENABLE = 0x0400    DCI_ENABLE              = 0x4000
+  GFCI_ENABLE             = 0x2000    GRID_ON_POWER_SS        = 0x0040
+  NEUTRAL_DETECT_ENABLE   = 0x0020    ANTI_ISLAND_ENABLE      = 0x0010
+  DRMS_ENABLE             = 0x0004    OVF_LOAD_DERATE_ENABLE  = 0x0002
 
 Register 110:
-  TAKE_LOAD_TOGETHER     = 0x0400
-  CHARGE_LAST            = 0x0010
+  TAKE_LOAD_TOGETHER = 0x0400    CHARGE_LAST = 0x0010
 
 Register 179:
-  ENABLE_PEAK_SHAVING    = 0x0080
+  ENABLE_PEAK_SHAVING = 0x0080
 """
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import CONF_ID, CONF_NAME, CONF_ICON, CONF_RESTORE_MODE
+from esphome.const import CONF_ID
 
 from . import (
     luxpower_sna_ns,
@@ -51,18 +43,16 @@ from . import (
 CONF_REGISTER = "register"
 CONF_BITMASK  = "bitmask"
 
-LuxpowerSNASwitch = luxpower_sna_ns.class_(
-    "LuxpowerSNASwitch", switch.Switch, cg.Component
-)
+# Switch already inherits Component – do NOT add cg.Component here
+LuxpowerSNASwitch = luxpower_sna_ns.class_("LuxpowerSNASwitch", switch.Switch)
 
-CONFIG_SCHEMA = cv.All(
-    switch.switch_schema(LuxpowerSNASwitch).extend(
-        LUXPOWER_SNA_COMPONENT_SCHEMA
-    ).extend({
-        cv.Required(CONF_REGISTER): cv.int_range(min=0, max=239),
-        cv.Required(CONF_BITMASK):  cv.hex_int,
-    })
-)
+# No cv.All() wrapper – extend directly so all keys (name, icon, ...) pass through
+CONFIG_SCHEMA = switch.switch_schema(LuxpowerSNASwitch).extend(
+    LUXPOWER_SNA_COMPONENT_SCHEMA
+).extend({
+    cv.Required(CONF_REGISTER): cv.int_range(min=0, max=239),
+    cv.Required(CONF_BITMASK):  cv.hex_int,
+})
 
 
 async def to_code(config):
