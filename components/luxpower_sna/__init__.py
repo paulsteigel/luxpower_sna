0e71c9269d0c8ced831826de7136e37fc4a71392
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2026-03-15 18:15:12 +0700

    Update __init__.py

--- components/luxpower_sna/__init__.py
+++ components/luxpower_sna/__init__.py
@@ -1,61 +1,51 @@
-# components/luxpower_sna/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.core import CORE, coroutine_with_priority
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_HOST, CONF_PORT, CONF_UPDATE_INTERVAL
 
-DEPENDENCIES = ["wifi"]
-AUTO_LOAD = ["sensor", "text_sensor"] # Make sure text_sensor is also loaded
+DEPENDENCIES = ["wifi", "network"]
+AUTO_LOAD = ["sensor", "text_sensor", "switch", "number"]
 MULTI_CONF = True
 
 luxpower_sna_ns = cg.esphome_ns.namespace("luxpower_sna")
-LuxpowerSNAComponent = luxpower_sna_ns.class_("LuxpowerSNAComponent", cg.PollingComponent)
+LuxpowerSNAComponent = luxpower_sna_ns.class_("LuxpowerSNAComponent", cg.Component)
 
 CONF_LUXPOWER_SNA_ID = "luxpower_sna_id"
+CONF_DONGLE_SERIAL = "dongle_serial"
+CONF_INVERTER_SERIAL = "inverter_serial"
+CONF_HOLD_UPDATE_INTERVAL = "hold_update_interval"
 
-LUXPOWER_SNA_COMPONENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_LUXPOWER_SNA_ID): cv.use_id(LuxpowerSNAComponent),
-    }
-)
+# Schema exported so platform files can extend it
+LUXPOWER_SNA_COMPONENT_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_LUXPOWER_SNA_ID): cv.use_id(LuxpowerSNAComponent),
+})
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(LuxpowerSNAComponent),
-            cv.Required("host"): cv.string,
-            cv.Required("port"): cv.port,
-            cv.Required("dongle_serial"): cv.string,
-            cv.Required("inverter_serial_number"): cv.string,
-        }
-    )
-    .extend(cv.polling_component_schema("20s"))
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(LuxpowerSNAComponent),
+    cv.Required(CONF_HOST): cv.string,
+    cv.Required(CONF_PORT): cv.port,
+    cv.Required(CONF_DONGLE_SERIAL): cv.string,
+    cv.Required(CONF_INVERTER_SERIAL): cv.string,
+    cv.Optional(CONF_UPDATE_INTERVAL, default="20s"): cv.update_interval,
+    cv.Optional(CONF_HOLD_UPDATE_INTERVAL, default="60s"): cv.update_interval,
+}).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_host(config["host"]))
-    cg.add(var.set_port(config["port"]))
+    cg.add(var.set_host(config[CONF_HOST]))
+    cg.add(var.set_port(config[CONF_PORT]))
 
-    # --- Corrected Code Generation ---
-    # We pass the string directly from the config.
-    # The C++ side will handle validation and conversion.
-    dongle_serial = config["dongle_serial"]
-    if len(dongle_serial) != 10:
-        raise cv.Invalid("dongle_serial must be 10 characters long")
-    cg.add(var.set_dongle_serial(dongle_serial))
+    dongle = config[CONF_DONGLE_SERIAL]
+    if len(dongle) != 10:
+        raise cv.Invalid("dongle_serial must be exactly 10 characters")
+    cg.add(var.set_dongle_serial(dongle))
 
-    inverter_serial = config["inverter_serial_number"]
-    if len(inverter_serial) != 10:
-        raise cv.Invalid("inverter_serial_number must be 10 characters long")
-    # This call must match the function name in the .h file exactly.
-    cg.add(var.set_inverter_serial(inverter_serial))
-    
-    #if CORE.is_esp32 or CORE.is_libretiny:
-    #    # https://github.com/ESP32Async/AsyncTCP
-    #    cg.add_library("ESP32Async/AsyncTCP", "3.4.4")
-    #elif CORE.is_esp8266:
-    #    # https://github.com/ESP32Async/ESPAsyncTCP
-    #    cg.add_library("ESP32Async/ESPAsyncTCP", "2.0.0")
+    inverter = config[CONF_INVERTER_SERIAL]
+    if len(inverter) != 10:
+        raise cv.Invalid("inverter_serial must be exactly 10 characters")
+    cg.add(var.set_inverter_serial(inverter))
+
+    cg.add(var.set_update_interval_ms(config[CONF_UPDATE_INTERVAL]))
+    cg.add(var.set_hold_update_interval_ms(config[CONF_HOLD_UPDATE_INTERVAL]))
