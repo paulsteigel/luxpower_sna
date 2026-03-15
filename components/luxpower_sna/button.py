e052b541a05cf6266e42df238c19ccef64b2a7a4
Đặng Đình Ngọc <paulsteigel@users.noreply.github.com>
2026-03-15 21:06:24 +0700

    Refactor LuxPower SNA button configuration and actions

--- components/luxpower_sna/button.py
+++ components/luxpower_sna/button.py
@@ -1,31 +1,56 @@
+"""
+LuxPower SNA – Button platform (JK BMS pattern).
+
+YAML usage:
+  button:
+    - platform: luxpower_sna
+      luxpower_sna_id: lux_hub
+      restart:
+        name: "Inverter Restart"
+      reset_all:
+        name: "Reset All Settings"
+"""
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
 from esphome.const import CONF_ID
 
 from . import luxpower_sna_ns, CONF_LUXPOWER_SNA_ID
-from . import LuxpowerSNAComponent  # noqa
 
-CONF_ACTION = "action"
-ACTIONS     = ["restart", "reset_all"]
+LuxpowerSNAComponent = luxpower_sna_ns.class_("LuxpowerSNAComponent", cg.Component)
+LuxpowerSNAButton    = luxpower_sna_ns.class_("LuxpowerSNAButton", button.Button, cg.Component)
 
-LuxpowerSNAButton = luxpower_sna_ns.class_("LuxpowerSNAButton", button.Button)
 ButtonAction = luxpower_sna_ns.enum("LuxpowerSNAButton::Action", is_class=True)
 
-ACTION_MAP = {
-    "restart":   ButtonAction.RESTART,
-    "reset_all": ButtonAction.RESET_ALL,
+# (action_enum, default_icon)
+BUTTONS = {
+    "restart":   (ButtonAction.RESTART,   "mdi:restart"),
+    "reset_all": (ButtonAction.RESET_ALL, "mdi:restore"),
 }
 
-CONFIG_SCHEMA = button.button_schema(LuxpowerSNAButton).extend({
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_LUXPOWER_SNA_ID): cv.use_id(LuxpowerSNAComponent),
-    cv.Required(CONF_ACTION):            cv.one_of(*ACTIONS, lower=True),
+    **{
+        cv.Optional(key): button.button_schema(
+            LuxpowerSNAButton,
+            icon=icon,
+        ).extend(cv.COMPONENT_SCHEMA)
+        for key, (_, icon) in BUTTONS.items()
+    },
 })
 
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_LUXPOWER_SNA_ID])
-    var = await button.new_button(config)
-    cg.add(var.set_parent(hub))
-    cg.add(var.set_action(ACTION_MAP[config[CONF_ACTION]]))
-    cg.add(hub.register_button(var))
+
+    for key, (action, _) in BUTTONS.items():
+        if key not in config:
+            continue
+        conf = config[key]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await button.register_button(var, conf)
+        cg.add(var.set_parent(hub))
+        cg.add(var.set_action(action))
+        cg.add(hub.register_button(var))
